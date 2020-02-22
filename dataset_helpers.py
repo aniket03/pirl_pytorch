fe846cc4d3f0ee9ef97d01ad67f6362a42194bde
Aniket <aniket.bhatnagar0310@gmail.com>
2020-02-22 16:59:42 -0500

    Add brightness jitter transform to images of CIFAR10 while training

--- dataset_helpers.py
+++ dataset_helpers.py
@@ -2,6 +2,7 @@ from torchvision.transforms import transforms
 
 def_train_transform = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
+    transforms.ColorJitter(brightness=[0.5, 1.5]),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
