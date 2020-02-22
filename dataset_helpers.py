ce4e1467f66694c6e31d00e5bbeda9b7d1dd1244
Aniket <aniket.bhatnagar0310@gmail.com>
2020-02-22 13:12:09 -0500

    Commiting train_test_helper dataset_helper main and resnet for training resnet18 on cifar10

---
+++ dataset_helpers.py
@@ -0,0 +1,12 @@
+from torchvision.transforms import transforms
+
+def_train_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+def_test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
