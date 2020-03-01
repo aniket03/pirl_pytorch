06c9dafb3c1520208b01e8d1377a9736108d8cef
Aniket <aniket.bhatnagar0310@gmail.com>
2020-03-01 16:05:08 -0500

    Train resnet from weights obtained of pre-text task

---
+++ network_helpers.py
@@ -0,0 +1,53 @@
+import torch
+
+from models import pirl_resnet18, classifier_resnet18
+
+
+def test_copy_weights(m1, m2):
+    """
+    Tests that weights copied from m1 into m2, are actually refected in m2
+    """
+    m1_state_dict = m1.state_dict()
+    m2_state_dict = m2.state_dict()
+    weight_copy_flag = 1
+    for name, param in m1_state_dict.items():
+        if name in m2_state_dict:
+            if not torch.all(torch.eq(param.data, m2_state_dict[name].data)):
+                print("Something is incorrect for layer {} in 2nd model", name)
+                weight_copy_flag = 0
+
+    if weight_copy_flag:
+        print('All is well')
+
+    return 1
+
+
+def copy_weights_between_models(m1, m2):
+    """
+    Copy weights for layers common between m1 and m2.
+    From m1 => m2
+    """
+
+    # Load state dictionaries for m1 model and m2 model
+    m1_state_dict = m1.state_dict()
+    m2_state_dict = m2.state_dict()
+
+    # Set the m2 model's weights with trained m1 model weights
+    for name, param in m1_state_dict.items():
+        if name not in m2_state_dict:
+            continue
+        else:
+            m2_state_dict[name] = param.data
+    m2.load_state_dict(m2_state_dict)
+
+    # Test that model m2 **really** has got updated weights
+    return test_copy_weights(m1, m2)
+
+
+if __name__ == '__main__':
+
+    pr = pirl_resnet18()
+    cr = classifier_resnet18(num_classes=10)
+
+    copy_success = copy_weights_between_models(pr, cr)
+
