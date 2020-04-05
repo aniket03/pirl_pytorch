0c53c1ebd526c2f0bb57ee2765ff98f7949d0368
Aniket <aniket.bhatnagar0310@gmail.com>
2020-04-05 18:17:54 -0400

    Include small constant epsilon when dividing feature vectors with feature norms

--- pirl_loss.py
+++ pirl_loss.py
@@ -11,14 +11,17 @@ def get_img_pair_probs(vi_batch, vi_t_batch, mn_arr, temp_parameter):
     :param temp_parameter: The temperature parameter
     """
 
+    # Define constant eps to ensure training is not impacted if norm of any image rep is zero
+    eps = 1e-6
+
     # L2 normalize vi, vi_t and memory bank representations
     vi_norm_arr = torch.norm(vi_batch, dim=1, keepdim=True)
     vi_t_norm_arr = torch.norm(vi_t_batch, dim=1, keepdim=True)
     mn_norm_arr = torch.norm(mn_arr, dim=1, keepdim=True)
 
-    vi_batch = vi_batch / vi_norm_arr
-    vi_t_batch = vi_t_batch/ vi_t_norm_arr
-    mn_arr = mn_arr / mn_norm_arr
+    vi_batch = vi_batch / (vi_norm_arr + eps)
+    vi_t_batch = vi_t_batch/ (vi_t_norm_arr + eps)
+    mn_arr = mn_arr / (mn_norm_arr + eps)
 
     # Find cosine similarities
     sim_vi_vi_t_arr = (vi_batch @ vi_t_batch.t()).diagonal()
@@ -32,7 +35,7 @@ def get_img_pair_probs(vi_batch, vi_t_batch, mn_arr, temp_parameter):
     sum_exp_sim_vi_t_mn_arr = torch.sum(exp_sim_vi_t_mn_mat, 1)
 
     # Find batch probabilities arr
-    batch_prob_arr = exp_sim_vi_vi_t_arr / (exp_sim_vi_vi_t_arr + sum_exp_sim_vi_t_mn_arr)
+    batch_prob_arr = exp_sim_vi_vi_t_arr / (exp_sim_vi_vi_t_arr + sum_exp_sim_vi_t_mn_arr + eps)
 
     return batch_prob_arr
 
